d1640192153eae26b33a6c81706438332ffc11ed
romaniv2511 <romaniv2511@gmail.com>
2022-10-13 17:08:57 +0300

    add project

---
+++ src/index.js
@@ -0,0 +1,86 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { ImagesApiService } from "./js/api-service";
+import { createMarkup } from "./js/createMarkup";
+import { refs } from "./js/refs";
+
+
+let lightbox = {};
+
+const imagesAPI = new ImagesApiService();
+
+const onFormSubmit = async (e) => {
+    e.preventDefault();
+    imagesAPI.resetPage();
+    refs.gallery.innerHTML = '';
+    refs.btnMore.classList.add('is-hidden');
+    const { elements: { searchQuery } } = e.currentTarget;
+
+    if (!searchQuery.value) {
+        Notify.info("You have not entered a query")
+        return;
+    }
+    imagesAPI.query = searchQuery.value.trim().replace(/ /ig, '+');
+
+    try {
+        const { hits, totalHits } = await imagesAPI.getImages();
+        if (!totalHits) {
+            Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+            return;
+        }
+        const markup = createMarkup(hits);
+        renderMarkup(markup);
+
+        Notify.success(`Hooray! We found ${totalHits} images.`)
+
+        lightbox = new SimpleLightbox('.gallery a')
+
+        imagesAPI.calculateTotalPages(totalHits);
+        if (imagesAPI.isShowLoadMore) {
+            refs.btnMore.classList.remove('is-hidden');
+        }
+        
+    } catch (error) {
+        onError(error)
+    }
+}
+refs.form.addEventListener('submit', onFormSubmit)
+
+const onLoadMoreClick = async () => {
+    const {hits} = await imagesAPI.getImages();
+    const markup = createMarkup(hits);
+    renderMarkup(markup);
+    lightbox.refresh();
+
+    getSmoothScrollAfterLoadMore();
+
+    if (!imagesAPI.isShowLoadMore) {
+        refs.btnMore.classList.add('is-hidden');
+        Notify.info("We're sorry, but you've reached the end of search results.")
+    }
+
+}
+refs.btnMore.addEventListener('click', onLoadMoreClick);
+
+
+function renderMarkup(markup) {
+    refs.gallery.insertAdjacentHTML('beforeend', markup)
+}
+
+function onError (error) {
+    console.log(error);
+    Notify.failure(`${error.message}`);
+}
+function getSmoothScrollAfterLoadMore () {
+    const { height: cardHeight } = document
+        .querySelector(".gallery")
+        .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+        top: cardHeight * 2,
+        behavior: "smooth",
+    });    
+}
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+    form: document.querySelector("#search-form"),
+    gallery: document.querySelector(".gallery"),
+    btnMore: document.querySelector(".load-more"),
+}
